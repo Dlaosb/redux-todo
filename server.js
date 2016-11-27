20da633a40d7e2274a1a72f56a74e0f8cdc27d95
Bryan Yee <yee.bryan.c@gmail.com>
2016-11-27 09:07:11 -0800

    Fix express.static server route and add 404 route

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const app = express();
 const path = require('path');
 const port = 8080; 
 
-app.use('/', express.static(path.join(__dirname, 'client')));  
+app.use(express.static(path.join(__dirname, 'client', 'dist')));  
+
+app.use((req, res) => {
+  res.end(404);
+});
 
 app.listen(port, ()=> {
   console.log(`I am listening on ${port}`); 
